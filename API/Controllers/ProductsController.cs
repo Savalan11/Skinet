f76115f8e9fbe8546d01fc70da669566d881359a
Savalan11 <savalan.suleymanli@gmail.com>
2024-02-09 16:44:41 +0100

    End of Section 8

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -40,7 +40,7 @@ namespace API.Controllers
             var totalItems = await _productsRepo.CountAsync(countSpec);
 
             var products = await _productsRepo.ListAsync(spec);
-
+           
             var data = _mapper
                 .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
